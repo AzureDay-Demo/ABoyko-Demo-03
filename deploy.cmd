fccfd3a9caf556f3b5771cb370ef615b6423698f
Boyko Anton <boyko.ant@live.com>
2015-03-14 04:39:18 +0200

    deploy 7

--- deploy.cmd
+++ deploy.cmd
@@ -102,7 +102,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   echo "npm install"
 
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
+  call :ExecuteCmd npm install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )

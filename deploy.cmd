9a1357b8890634a38ed46839224c0c4d2a8049b1
Boyko Anton <boyko.ant@live.com>
2015-03-14 04:17:12 +0200

    adjust deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -63,7 +63,7 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
     SET /p NODE_EXE=<"%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
   )
-  
+
   IF EXIST "%DEPLOYMENT_TEMP%\__npmVersion.tmp" (
     SET /p NPM_JS_PATH=<"%DEPLOYMENT_TEMP%\__npmVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
@@ -104,6 +104,27 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+
+:: 4. Install bower packages
+IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd bower install
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 5. Build
+IF EXIST "%DEPLOYMENT_TARGET%\gruntfile.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd grunt build
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 6. Cleanup
+
+
+:: 7. Deploy
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
